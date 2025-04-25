c42c5d38fd53012f6967b4b77b59803cf2f32e96
Josh Del Favero <josh.delfavero@tcgplayer.com>
2025-04-25 13:04:53 -0400

    Move JS code to external resource

---
+++ src/Retro95/wwwroot/js/main.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function () {
+  updateClock();
+  startClock();
+});
+
+function startClock() {
+  const now = new Date();
+  setTimeout(function () {
+    updateClock();
+    setInterval(() => updateClock(), 60 * 1000);
+  }, (60 - now.getSeconds()) * 1000);
+}
+
+function updateClock() {
+  const now = new Date();
+
+  let hour = now.getHours();
+  const meridiem = hour >= 12 ? "PM" : "AM";
+  hour = hour % 12 || 12;
+
+  let minute = now.getMinutes();
+  minute = minute < 10 ? `0${minute}` : minute.toString(10);
+
+  document.getElementById("clock").textContent = `${hour}:${minute} ${meridiem}`;
+}

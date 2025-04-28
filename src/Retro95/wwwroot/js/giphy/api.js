64a587c91c5e80eb3806bf268deceaa5a65ba0fa
Josh Del Favero <josh.delfavero@tcgplayer.com>
2025-04-28 14:08:01 -0400

    Return just the array of images with original and downsample

--- src/Retro95/wwwroot/js/giphy/api.js
+++ src/Retro95/wwwroot/js/giphy/api.js
@@ -1,10 +1,12 @@
 const hostUrl = 'https://api.giphy.com/v1/gifs/search'
 
 const search = async (options) => {
-  const params = URLSearchParams(options)
+  const { data } = await fetch(`${hostUrl}${URLSearchParams(options).toString()}`)
 
-  return await fetch(`{hosturl}${params.toString()}`
-  )
+  return data.map(({ images: { original, fixed_height_downsampled: wide, fixed_width_downsampled: tall } }) => ({
+    original: original.webp,
+    downsampled: parseInt(wide.size) > parseInt(tall.size) ? wide.webp : tall.webp
+  }))
 }
 
 export default {
